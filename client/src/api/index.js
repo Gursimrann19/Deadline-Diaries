20e805b741c0c3529fca2c0228c0008e7bb7a525
Gursimran <Gursimrann19@gmail.com>
2026-05-06 10:31:02 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://deadline-diaries-production.up.railway.app' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('sf_token');
