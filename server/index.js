01c218ace324b628fa9d23eca11ee6a91d9e1f8d
Gursimran <Gursimrann19@gmail.com>
2026-05-06 12:08:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
+app.use(cors({
+    origin: 'https://deadline-diaries.vercel.app',
+    credentials: true
+}));
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
