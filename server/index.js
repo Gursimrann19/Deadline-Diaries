9cfdbee22d817cd23ce9553ba1b2da603c3f70cf
Gursimran <Gursimrann19@gmail.com>
2026-05-06 10:36:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,8 +5,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-app.use(express.json());
+app.use(cors({ origin: 'https://deadlinediaries.netlify.app/', credentials: true }));app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/auth',        require('./routes/auth'));
